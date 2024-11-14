130c46019f96eb04ea6dcfb2b6e2d77e47246862
n0wsh <galbadrakhtergel0820@gmail.com>
2024-11-14 16:31:41 +0800

    header-top added

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var root = document.querySelector(":root");
 let header = document.getElementById("header");
+let headerTop = document.getElementById("header-top");
 let headerLogo = document.getElementById("header-logo");
 let contactUs = document.getElementById("contact-us");
 
 window.onscroll = () => {
   if (window.scrollY == 0) {
     root.style.setProperty("--header", "#fff");
-    headerLogo.setAttribute("data-expanded", "true");
     header.setAttribute("data-transparent", "true");
+    headerLogo.setAttribute("data-expanded", "true");
+    headerTop.setAttribute("data-hidden", "false");
     contactUs.style.opacity = 0;
     contactUs.style.pointerEvents = "unset";
     contactUs.style.cursor = "auto";
   } else {
     root.style.setProperty("--header", "#000");
-    headerLogo.setAttribute("data-expanded", "false");
     header.setAttribute("data-transparent", "false");
+    headerLogo.setAttribute("data-expanded", "false");
+    headerTop.setAttribute("data-hidden", "true");
     contactUs.style.opacity = 1;
     contactUs.style.pointerEvents = "auto";
     contactUs.style.cursor = "pointer";
